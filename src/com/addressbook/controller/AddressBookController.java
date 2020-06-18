5b9af5d30475132cc10c2d8ceddc5f4cd3794985
Seetharamulu T <thumurugoti.seetharamulu@gmail.com>
2020-06-18 23:48:32 +0530

    Added: edit a person information

--- src/com/addressbook/controller/AddressBookController.java
+++ src/com/addressbook/controller/AddressBookController.java
@@ -1,15 +1,72 @@
 package com.addressbook.controller;
 import com.addressbook.model.*;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AddressBookController {
-    Scanner Sc=new Scanner(System.in);
+    Person person = new Person();
 
-    public void addPerson(){
-        Person person = new Person();
-        System.out.println("enter firstname");
+    Scanner Sc = new Scanner(System.in);
+    LinkedList<Person> personList = new LinkedList<Person>();
+
+    public Person getPerson() {
+        System.out.println("enter first name");
+        String firstName = Sc.next();
+        System.out.println("enter last name");
+        String lastName = Sc.next();
+
+        for (Person person : this.personList) {
+            if (firstName.equalsIgnoreCase(person.getFirstName()) && lastName.equalsIgnoreCase(person.getLastName())) {
+                return person;
+            }
+        }
+        return null;
+    }
+
+    public void addPerson() {
+
+        System.out.println("enter first name");
         person.setFirstName(Sc.next());
 
+        System.out.println("enter last name");
+        person.setLastName(Sc.next());
+
+        System.out.println("enter address");
+        person.setAddress(Sc.next());
+
+        System.out.println("enter city");
+        person.setCity(Sc.next());
+
+        System.out.println("enter state");
+        person.setState(Sc.next());
+
+        System.out.println("enter zipcode");
+        person.setZip(Sc.next());
+
+        System.out.println("enter phone number");
+        person.setPhoneNumber(Sc.next());
+
+        System.out.println("added person successfully");
+        personList.add(person);
+    }
+
+    public void editPerson() {
+
+        Person persontoedit = this.getPerson();
+        if (persontoedit != null) {
+            System.out.println("enter address");
+            persontoedit.setAddress(Sc.next());
+            System.out.println("enter city");
+            persontoedit.setCity(Sc.next());
+            System.out.println("enter state");
+            persontoedit.setState(Sc.next());
+            System.out.println("enter zip code");
+            persontoedit.setZip(Sc.next());
+            System.out.println("enter phone number");
+            persontoedit.setPhoneNumber(Sc.next());
+        }
     }
 }
--- src/com/addressbook/test/AddressBook.java
+++ src/com/addressbook/test/AddressBook.java
@@ -2,12 +2,33 @@ package com.addressbook.test;
 import com.addressbook.controller.*;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class AddressBook {
     public static void main(String[] args) {
-     AddressBookController addressBookController = new AddressBookController();
 
-        System.out.println("Welcome To Address Book Program");
-        addressBookController.addPerson();
+        Scanner Sc=new Scanner(System.in);
+
+        AddressBookController addressBookController = new AddressBookController();
+
+        while(true) {
+            System.out.println("Welcome To Address Book Program");
+
+            System.out.println("Choose what to do");
+            System.out.println("1) add a person");
+            System.out.println("2) edit person");
+            System.out.println("3) exit");
+            int choice = Sc.nextInt();
+            switch (choice) {
+                case 1:
+                    addressBookController.addPerson();
+                    break;
+                case 2:
+                    addressBookController.editPerson();
+                    break;
+                    case 3: return ;
+            }
+        }
+
     }
 }
