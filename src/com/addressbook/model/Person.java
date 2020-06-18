5a3cd891359bff3fcbbf476220613a2ff96b2335
Seetharamulu T <thumurugoti.seetharamulu@gmail.com>
2020-06-18 20:59:10 +0530

    Added : create contact in addressbook

---
+++ src/com/addressbook/model/Person.java
@@ -0,0 +1,67 @@
+package com.addressbook.model;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private int zip;
+    private String phoneNumber;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public int getZip() {
+        return this.zip;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+}
